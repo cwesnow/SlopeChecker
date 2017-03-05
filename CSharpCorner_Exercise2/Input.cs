b0d6a30c83171734eddc8646f4cb0e7e9acc1982
cwesnow2000 <cwesnow2000@yahoo.com>
2017-03-04 20:20:17 -0600

    initial

---
+++ CSharpCorner_Exercise2/Input.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharpCorner_Exercise2
+{
+    class Input
+    {
+        /// <summary>
+        /// Allows for user input with a custom prompt that allows a minimum and maximum number range to be returned
+        /// </summary>
+        /// <param name="prompt">Console output for user</param>
+        /// <param name="minimum">Smallest value allowed</param>
+        /// <param name="maximum">Largest value allowed</param>
+        /// <returns></returns>
+        internal static int getInt(string prompt, int minimum, int maximum)
+        {
+            int x;
+            Console.WriteLine(prompt);
+            while (!int.TryParse(Console.ReadLine(), out x) || x < minimum || x > maximum)
+                Console.WriteLine("Error: Number must be between {0} and {1}", minimum, maximum);
+            return x;
+        }
+
+        /// <summary>
+        /// User enters X and Y values then returns Point
+        /// </summary>
+        /// <param name="slopeNumber"></param>
+        /// <returns></returns>
+        internal static Point getPoint(int pointNumber)
+        {
+            Console.WriteLine("Point # {0}", pointNumber);
+
+            Point point = new Point();
+            point.x = getInt("  X-coordinate >>> ", -100, 100);
+            point.y = getInt("  Y-coordinate >>> ", -100, 100);
+
+            // Check for correct input, before returning new values
+            Console.WriteLine("X = {0} and Y = {1}, is this correct? [Y,N]", point.x, point.y);
+            if (!(Console.ReadLine().Substring(0, 1).ToUpper() == "Y")) point = getPoint(pointNumber);
+
+            return point;
+        }
+    }
+}
---
+++ CSharpCorner_Exercise2/Slopes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCorner_Exercise2
+{
+    class Slopes
+    {
+        internal static List<Point> points = new List<Point>();
+
+        public static void addPoints(int numberOfPoints)
+        {
+            // Collection of Slope inputs
+            for (int x = 0; x < numberOfPoints; x++)
+            {
+                addPoint();
+            }
+        }
+        
+        public static void addPoint(){
+            points.Add( Input.getPoint(points.Count+1) );
+        }
+
+        public static string outputString()
+        {
+            var sb = new StringBuilder("");
+            int count = 0;
+
+            // Output each Slopes Points and Slope
+            foreach (var point in points)
+            {
+                count++;
+                sb.Append("Point " + count + " (" + point.x + ", " + point.y + ")\n");
+            }
+            return sb.ToString();
+        }
+
+
+        // Slope = delta y over delta x OR ( y2 - y1 ) / (x2 - x1)
+        public static bool isSameLine()
+        {
+            if (points.Count < 2) throw new ArgumentOutOfRangeException();
+
+            double division = points[1].x - points[0].x;
+            if (division == 0) { throw new DivideByZeroException(); }
+
+            // Slope of point 0 and 1
+            double Slope1 = (points[1].y - points[0].y) / division;
+
+            // Iterates through each Point.
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                double Slope2 = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
+                if (Slope2 != Slope1) return false;
+            }
+
+            return true;
+        }
+
+    }
+}
