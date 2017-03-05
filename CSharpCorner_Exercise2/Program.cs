2e1a4b92a60995965721384d61fa9072d36b4ae5
cwesnow2000 <cwesnow2000@yahoo.com>
2017-03-05 11:07:56 -0600

    Minor updates - comment for 3x requirement

--- CSharpCorner_Exercise2/Program.cs
+++ CSharpCorner_Exercise2/Program.cs
@@ -9,10 +9,15 @@ namespace CSharpCorner_Exercise2
 
             setup();
 
-            // Add # of points based on user input, Ranging from 2 to 5
+            // Original Requirement for 3x points
+            //Slopes.addPoint();
+            //Slopes.addPoint();
+            //Slopes.addPoint();
+
+            // This allows variable amounts of inputs, and uses getInt to set a min and max range for it.
             Slopes.addPoints( Input.getInt("How many points?", 2, 5) );
             
-            // Display current points
+            // Display all entered Points - User gets a final look at the data before it's checked
             Console.WriteLine(Slopes.outputString());
 
             // Slope check - Throws errors, display useful message
@@ -23,17 +28,17 @@ namespace CSharpCorner_Exercise2
                 else {
                     Console.WriteLine("Points are not on the same line."); }
             }
-            catch (ArgumentOutOfRangeException)
-            {
-                Console.WriteLine("Error: Requires 2 points to find a slope.");
-            }
             catch (DivideByZeroException)
             {
                 Console.WriteLine("Error: Undefined line, Failed vertical line test");
             }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Error: Requires 2 points to find a slope.");
+            }
             catch (Exception e)
             {
-                Console.WriteLine( e.Message );
+                Console.WriteLine( "Error: {0}", e.Message );
             }
             finally
             {
