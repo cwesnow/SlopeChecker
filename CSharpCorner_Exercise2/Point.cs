16bb0fccaa592b89916aa5b1fe22e8a6360b0aa0
cwesnow2000 <cwesnow2000@yahoo.com>
2017-03-05 19:50:06 -0600

    Point - Comment, variable, constructor edits

--- CSharpCorner_Exercise2/Point.cs
+++ CSharpCorner_Exercise2/Point.cs
@@ -1,13 +1,15 @@
 ﻿namespace CSharpCorner_Exercise2
 {
+    /// <summary>
+    /// Represents (X,Y) coordinates
+    /// </summary>
     struct Point
     {
-        public int x;
-        public int y;
+        public int x, y;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        public Point(int newX, int newY) {
+            x = newX;
+            y = newY;
         }
     }
 }
